81e5b65512ed95e203e43e1f9a3f6d847ef7b51e
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-07-04 00:47:09 -0300

    feat: Create LinkedList methods sketch

--- data-structure/linked-list/linked-list.js
+++ data-structure/linked-list/linked-list.js
@@ -1,3 +1,36 @@
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  shift() {}
+
+  unshift() {}
+
+  push() {}
+
+  pop() {}
+
+  splice() {}
+
+  getValue() {}
+
+  getNode() {}
+
+  contains() {}
+
+  isEmpty() {}
+
+  split() {}
+
+  reverse() {}
+
+  clear() {}
+
+  printValues() {}
+}
+
 class Node {
   constructor(value, next = null) {
     this.value = value;
