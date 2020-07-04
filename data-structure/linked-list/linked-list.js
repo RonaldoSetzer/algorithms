e666bba288ce0483dcb8bdd629e9b264663cdade
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-07-04 19:38:34 -0300

    feat: Implement push/pop methods to linked-list

--- data-structure/linked-list/linked-list.js
+++ data-structure/linked-list/linked-list.js
@@ -14,9 +14,39 @@ class LinkedList {
     this.size++;
   }
 
-  push() {}
+  push(value) {
+    if (!this.head) {
+      this.unshift(value);
+      return;
+    }
+    let tail = this.head;
+    while (tail.next) {
+      tail = tail.next;
+    }
+    tail.next = new Node(value);
+    this.size++;
+  }
 
-  pop() {}
+  pop() {
+    if (this.size === 1) {
+      this.shift();
+      this.size--;
+      return;
+    }
+
+    this.size--;
+    let tail = this.head;
+    let count = 1;
+
+    while (tail.next) {
+      if (count === this.size) {
+        tail.next = null;
+        return;
+      }
+      tail = tail.next;
+      count++;
+    }
+  }
 
   splice() {}
 
--- data-structure/linked-list/linked-list.spec.js
+++ data-structure/linked-list/linked-list.spec.js
@@ -2,6 +2,67 @@ const { Node, LinkedList } = require('./linked-list');
 
 describe('LinkedList', () => {
   describe('LinkedList', () => {
+    test('expect to pop remove head', () => {
+      const ll = new LinkedList();
+      ll.push(1);
+      ll.pop();
+
+      expect(ll.split()).toEqual([]);
+    });
+
+    test('expect to pop to remove the tail value', () => {
+      const ll = new LinkedList();
+      ll.push(1);
+      ll.push(2);
+      ll.push(3);
+      ll.pop();
+
+      expect(ll.split()).toEqual([1, 2]);
+    });
+
+    test('expect to pop to decrement size', () => {
+      const ll = new LinkedList();
+      ll.push(1);
+      ll.push(2);
+      ll.push(3);
+      ll.pop();
+
+      expect(ll.size).toEqual(2);
+    });
+
+    test('expect to push to increment size', () => {
+      const ll = new LinkedList();
+      ll.push(1);
+      ll.push(2);
+      ll.push(3);
+
+      expect(ll.size).toEqual(3);
+    });
+
+    test('expect to push add a node to the tail position', () => {
+      const ll = new LinkedList();
+      ll.push(1);
+      ll.push(2);
+      ll.push(3);
+
+      expect(ll.split()).toEqual([1, 2, 3]);
+    });
+
+    test('expect to push add a node next head', () => {
+      const ll = new LinkedList();
+      ll.push(1);
+      ll.push(2);
+
+      expect(ll.head.next.value).toEqual(2);
+    });
+
+    test('expect to push adds head when does not have one', () => {
+      const ll = new LinkedList();
+      ll.push(1);
+
+      expect(ll.head.value).toEqual(1);
+    });
+
     test('expect to remove the head and set head.next to the new head', () => {
       const ll = new LinkedList();
       ll.unshift(1);
